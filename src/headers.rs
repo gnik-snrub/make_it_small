ce39b7d7ef26ed31b7cb951ca958a7587fd3e09f
Josiah Morris <josiahbmorris@hotmail.com>
2025-07-16 03:36:36 +0900

    Added original_size evaluation to write_header function

--- src/headers.rs
+++ src/headers.rs
@@ -12,12 +12,12 @@ pub struct Headers {
     salt_and_iv: [usize; 2],
 }
 
-pub fn write_header() -> Headers {
+pub fn write_header(file: &Vec<u8>) -> Headers {
     Headers {
         magic_bytes: MAGIC_BYTES,
         version: VERSION,
         flags: 0b0000_0000, // TODO - Create a bitmask from selected flags using consts
-        original_size: 0, // TODO - Check input file sizes, and put here
+        original_size: file.len() as u64, // TODO - Check input file sizes, and put here
         compressed_size: 0, // TODO - Check output file sizes, and put here
         // TODO - Randomize both values with a secure RNG
         salt_and_iv: [1234432112344321, 4321123443211234]
