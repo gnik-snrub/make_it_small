9b827bf5914d632638db057614953a84c9e4d461
Josiah Morris <josiahbmorris@hotmail.com>
2025-07-17 05:09:18 +0900

    Added new() write_bits() and flush() functions to BitWriter

--- src/io.rs
+++ src/io.rs
@@ -30,3 +30,38 @@ struct BitWriter {
     padding_bits: u8,
 }
 
+impl BitWriter {
+    fn new() -> BitWriter {
+        BitWriter { buffer: 0, len: 0, output: vec![], padding_bits: 0 }
+    }
+
+    fn write_bits(&mut self, value: u32, bit_count: u8) {
+        if bit_count == 0 {
+            return;
+        }
+        for i in (0..bit_count).rev() {
+            let bit = (value >> i) & 1;
+            self.buffer <<= 1;
+            self.buffer |= bit as u8;
+            self.len += 1;
+
+            if self.len == 8 {
+                self.output.push(self.buffer);
+                self.buffer = 0;
+                self.len = 0;
+            }
+        }
+    }
+
+    fn flush(&mut self) {
+        for _ in 0..(8 - self.len) {
+            self.buffer <<= 1;
+            self.buffer |= 0 as u8;
+            self.padding_bits += 1;
+        }
+        self.output.push(self.buffer);
+        self.buffer = 0;
+        self.len = 0;
+    }
+}
+
