522c58bc3ff52fb0bc015f376060bf5561ea3db2
Josiah Morris <josiahbmorris@hotmail.com>
2025-07-19 04:30:27 +0900

    Added to_bytes function implementation to Headers struct

--- src/headers.rs
+++ src/headers.rs
@@ -27,3 +27,22 @@ pub fn write_header(file: &Vec<u8>, name: &str, padding_bits: u8) -> Headers {
         padding_bits
     }
 }
+
+impl Headers {
+    pub fn to_bytes(self) -> Vec<u8> {
+        let mut bytes = vec![];
+
+        bytes.extend_from_slice(&self.magic_bytes);
+        bytes.extend_from_slice(&self.version.to_le_bytes());
+        bytes.extend_from_slice(&self.flags.to_le_bytes());
+        bytes.extend_from_slice(&self.original_size.to_le_bytes());
+        bytes.extend_from_slice(&(self.original_file_name.len() as u16).to_le_bytes());
+        bytes.extend_from_slice(&self.original_file_name.as_bytes());
+        bytes.extend_from_slice(&self.compressed_size.to_le_bytes());
+        bytes.extend_from_slice(&(self.salt_and_iv[0] as u64).to_le_bytes());
+        bytes.extend_from_slice(&(self.salt_and_iv[1] as u64).to_le_bytes());
+        bytes.extend_from_slice(&self.padding_bits.to_le_bytes());
+
+        bytes
+    }
+}
