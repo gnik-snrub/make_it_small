78635d7442e7f91b1fd82987124cf159383f7413
Josiah Morris <josiahbmorris@hotmail.com>
2025-07-19 04:29:46 +0900

    Added padding bits to .small file headers

--- src/headers.rs
+++ src/headers.rs
@@ -10,10 +10,11 @@ pub struct Headers {
     original_size: u64,
     original_file_name: String,
     compressed_size: u64,
-    salt_and_iv: [usize; 2],
+    salt_and_iv: [u64; 2],
+    padding_bits: u8,
 }
 
-pub fn write_header(file: &Vec<u8>, name: &str) -> Headers {
+pub fn write_header(file: &Vec<u8>, name: &str, padding_bits: u8) -> Headers {
     Headers {
         magic_bytes: MAGIC_BYTES,
         version: VERSION,
@@ -22,6 +23,7 @@ pub fn write_header(file: &Vec<u8>, name: &str) -> Headers {
         original_file_name: name.to_string(),
         compressed_size: 0, // TODO - Check output file sizes, and put here
         // TODO - Randomize both values with a secure RNG
-        salt_and_iv: [1234432112344321, 4321123443211234]
+        salt_and_iv: [1234432112344321, 4321123443211234],
+        padding_bits
     }
 }
