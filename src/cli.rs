51b0cf73d91638ce06e94d7211def41506844b8d
Josiah Morris <josiahbmorris@hotmail.com>
2025-07-20 06:02:26 +0900

    Tweaked cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -20,26 +20,32 @@ enum Command {
     },
     Decompress {
         name_in: String,
-        name_out: String,
     }
 }
 
 pub fn run_command() {
     let tokens = Cli::parse();
     match tokens {
-        Cli { command: Some(Command::Compress { name_in, name_out }) } => {
+        Cli { command: Some(Command::Compress { name_in, mut name_out }) } => {
             let file = read_file(&name_in);
             let name = Path::new(&name_in).file_name().unwrap().to_str().unwrap();
             let headers = write_header(&file, name, 0);
             let mut small_file = headers.to_bytes();
             small_file.extend_from_slice(&file);
+            name_out.push_str(".small");
             write_file(small_file, &name_out);
         },
-        Cli { command: Some(Command::Decompress { name_in, name_out }) } => {
+        Cli { command: Some(Command::Decompress { name_in }) } => {
             let file = read_file(&name_in);
-            let (headers, header_length) = Headers::from_bytes(&file);
-            println!("Headers: {:?}", headers);
-            write_file(file[header_length..].to_vec(), &name_out);
+            let (headers, header_length) = match Headers::from_bytes(&file) {
+                Ok(header_and_length) => {
+                    header_and_length
+                }
+                Err(e) => {
+                    panic!("Error deserializing headers: {e}");
+                }
+            };
+            write_file(file[header_length..].to_vec(), &headers.original_file_name);
         },
         Cli { command: None } => {
 
