a64b866e34d9cb652056963eaf00836960c85d76
Josiah Morris <josiahbmorris@hotmail.com>
2025-07-22 04:20:21 +0900

    Added Node struct and Ord Eq implementations

--- src/huffman.rs
+++ src/huffman.rs
@@ -8,4 +8,30 @@ pub fn compute_frequencies(buf: &[u8]) -> [u64; 256] {
     freq
 }
 
+pub struct Node {
+    pub weight: u64,
+    pub symbol: Option<u8>,
+    pub left: Option<Box<Node>>,
+    pub right: Option<Box<Node>>,
+}
 
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.weight == other.weight
+    }
+}
+
+impl Eq for Node {
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Node) -> Ordering {
+        other.weight.cmp(&self.weight)
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
