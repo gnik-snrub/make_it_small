0e17edfb96c0eda3b4318c0fbe0fa4ea1c9aa83a
Josiah Morris <josiahbmorris@hotmail.com>
2025-08-02 04:41:20 +0900

    Added constants for stored_raw and is_archive

--- src/constants.rs
+++ src/constants.rs
@@ -2,8 +2,10 @@ pub const MAGIC_BYTES: [u8; 4] = *b"SMAL";
 pub const VERSION: u8 = 1;
 
 // Flags
-pub const FLAG_ENCRYPTED: u8 = 0b0000_0001;
-pub const FLAG_HAS_CHECKSUM: u8 = 0b0000_0010;
+pub const ENCRYPTED: u8         = 0b0000_0001; // TODO
+pub const HAS_CHECKSUM: u8      = 0b0000_0010; // TODO
+pub const STORED_RAW: u8        = 0b0000_0100;
+pub const IS_ARCHIVE: u8        = 0b0000_1000; // TODO
 
 // Sizes
 pub const SALT_LENGTH: usize = 16;
